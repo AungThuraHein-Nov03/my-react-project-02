c80f51d8ec121e581c51e185494625772bf2c090
AungThuraHein <u6726135@au.edu>
2025-12-26 16:17:47 +0700

    second assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Register from "./Register";
+import Register from "./component/Register";
+import Registeration_form from "./component/Registeration_form"
 import "./App.css";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Register />} />
+      <Route path="/register-form" element = {<Registeration_form/>}/>
     </Routes>
   );
 } 
